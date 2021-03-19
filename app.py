6f23687fabae07c3e71b062745d9d9422c4505d0
Nick Battista <nickbattista@Nick.Battista>
2021-03-19 16:46:35 -0600

    updated forecast plot rendering

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from fbprophet.plot import plot_plotly, plot_components_plotly
 import pandas as pd
 import plotly
 import plotly.express as px
+import plotly.graph_objects as go
 import json
 
 
@@ -45,7 +46,7 @@ def render_plot(filename):
     fig = px.line(df, x = 'ds', y = 'y', title='Data')
     plot_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-    # Summary statistics
+    # Summarize dataset statistics
     min = round(df['y'].min(),2)
     max = round(df['y'].max(),2)
     mean = round(df['y'].mean(),2)
@@ -66,15 +67,55 @@ def generate_forecast(filename):
     m.fit(df)
 
     forecast_length = int(request.form['future'])
-
     future = m.make_future_dataframe(periods=forecast_length)
     future.tail()
-
     forecast = m.predict(future)
-    fig = px.line(forecast, x = 'ds', y = 'yhat', title='Data')
+
+    yhat = go.Scatter(
+        name = 'forecast',
+        x = forecast['ds'],
+        y = forecast['yhat'],
+        mode='lines',
+        line=dict(color='rgb(31, 119, 180)')
+    )
+
+    yhat_upper = go.Scatter(
+        name='Upper Bound',
+        x=forecast['ds'],
+        y=forecast['yhat_upper'],
+        mode='lines',
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        showlegend=False     
+    )
+
+    yhat_lower = go.Scatter(
+        name='Lower Bound',
+        x=forecast['ds'],
+        y=forecast['yhat_lower'],
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        mode='lines',
+        fillcolor='rgba(68, 68, 68, 0.3)',
+        fill='tonexty',
+        showlegend=False
+    )  
+
+    actual = go.Scatter(
+        name = 'actuals',
+        x = forecast['ds'],
+        y = df['y'],
+        mode='markers',
+        line=dict(color='rgb(0,0,0)')
+    )
+
+    fig = go.Figure(actual)
+
+    fig.add_trace(yhat)
+    fig.add_trace(yhat_upper)
+    fig.add_trace(yhat_lower)
 
     forecast_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
     return render_template('review_forecast.html', forecast_json=forecast_json)
 
 
@@ -82,6 +123,7 @@ def generate_forecast(filename):
 @app.route("/export_data/<filename>")
 def export_data(filename):
     """Export forecast data."""
-    filename = "yosemite_temps"
-    df = pd.read_csv('/Users/nickbattista/Desktop/html-plot/static/file_uploads/example_yosemite_temps.csv')
+    filename = filename
+    df = pd.read_csv('/Users/nickbattista/Desktop/html-plot/static/{filename}.csv')
     df.to_csv(f"/Users/nickbattista/Downloads/{filename}.csv")
+
